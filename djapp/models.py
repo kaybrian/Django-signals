543603398f045efe62d1e26a4bdb2385576f9dde
kaybrian <kayonbrian@gmail.com>
2020-06-13 19:17:15 +0300

    created the post model and its signal

--- djapp/models.py
+++ djapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.signals import post_save,pre_save
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+def save_post(sender,instance,**kwargs):
+    print("A new post post have been saved")
+
+post_save.connect(save_post,sender=Post)
