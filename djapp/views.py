63b026513ce5222767a837ae6272a98713937b24
kaybrian <kayonbrian@gmail.com>
2020-06-13 19:38:31 +0300

    custom signals

--- djapp/views.py
+++ djapp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.signals import request_finished
-from django.dispatch import receiver
+from django.dispatch import receiver,Signal
+from .models import Post 
+
+
+request_counter_signal = Signal(providing_args=['timestamp'])
+
 
 def home(request):
+    request_counter_signal.send(sender=Post ,timestamp='its now')
     return HttpResponse("You have request the home page")
 
 @receiver(request_finished)
 def home_page_requested(sender,**kwargs):
     print('The home page has been request and its fine')
+
+@receiver(request_counter_signal)
+def post_counter(sender,**kwargs):
+    print(kwargs)
